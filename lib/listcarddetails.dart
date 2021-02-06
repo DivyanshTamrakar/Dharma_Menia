2a3162f8aa7e315fd6a5f655009a5847c0aeff11
DivyanshTamrakar <tamrakardivyansh189@gmail.com>
2021-02-06 16:53:37 +0530

    feat : list detail screen added

--- lib/listcarddetails.dart
+++ lib/listcarddetails.dart
@@ -5,8 +5,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 class ListCardInformation extends StatefulWidget {
-
-
   _ListCardInformationState createState() => _ListCardInformationState();
 }
 
@@ -19,118 +17,128 @@ class _ListCardInformationState extends State<ListCardInformation> {
   @override
   void initState() {
     super.initState();
-
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar:
-      AppBar(backgroundColor: Colors.transparent, elevation: 0.0, actions: [
-        SizedBox(
-            width: 50.0,
-            child: GestureDetector(
-                onTap: () {
-                  print("CLicked Favorite");
-                },
-                child: Icon(Icons.favorite_border_outlined))),
-        SizedBox(
-            width: 50.0,
-            child: GestureDetector(
-                onTap: () {
-                  print("CLicked Share");
-                },
-                child: Icon(Icons.share))),
-      ]),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            Image.asset("assets/ganeshji.png"),
-            Container(
-              margin: EdgeInsets.only(top: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Image.asset("assets/brah.png"),
-                  Text(
-                    "Hindusim",
-                    style: TextStyle(
-                        fontSize: 25.0,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  RaisedButton(
-                    color: Colors.deepOrange,
-                    elevation: 10.0,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    onPressed: () {},
-                    child: Text(
-                      "Learn More",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-                margin: EdgeInsets.all(12.0),
-                child: ExpandableText(
-                  longText,
-                  expandText: 'show more',
-                  collapseText: 'show less',
-                  maxLines: 9,
-                  linkColor: Colors.blue,
-                  style: TextStyle(fontSize: 18.0),
-                  textAlign: TextAlign.justify,
-                )),
-            Divider(
-              color: Colors.black,
-              endIndent: 10.0,
-              indent: 12.0,
-            ),
-            Container(
-
-              margin: EdgeInsets.only(left: 20.0, right: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            toolbarHeight: 150.0,
+            actions: [
+              Column(
                 children: [
                   SizedBox(
-                      width: MediaQuery.of(context).size.width / 2 - 24,
+                      width: 50.0,
+                      height: 50.0,
+                      child: GestureDetector(
+                          onTap: () {
+                            print("CLicked Share");
+                          },
+                          child: Icon(Icons.share))),
+                  SizedBox(
+                      width: 50.0,
+                      height: 50.0,
+                      child: GestureDetector(
+                          onTap: () {
+                            print("CLicked Speaker");
+                          },
+                          child: Icon(Icons.volume_down))),
+                  SizedBox(
+                      width: 50.0,
+                      height: 50.0,
+                      child: GestureDetector(
+                          onTap: () {
+                            print("CLicked Favorite");
+                          },
+                          child: Icon(Icons.favorite_border_outlined))),
+                ],
+              ),
+            ]),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              Image.asset("assets/ganeshji.png"),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Image.asset(
+                      "assets/brah.png",
+                      height: 80.0,
+                      width: 80.0,
+                    ),
+                    Text(
+                      "Brahmins",
+                      style: TextStyle(
+                          fontSize: 25.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 4,
                       child: RaisedButton(
-                        color: button_one ? Colors.lightBlueAccent : Colors.grey,
+                        color: Colors.lightBlue[900],
+                        elevation: 10.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
                         onPressed: () {
-
-                          setState(() {
-                            button_one = !button_one;
-                          });
                           Navigator.of(context).push(MaterialPageRoute(
                               builder: (BuildContext context) => QuizScreen()));
                         },
-                        child: Text("Culture"),
-                      )),
-                  SizedBox(
-                      width: MediaQuery.of(context).size.width / 2 - 24,
-                      child: RaisedButton(
-                        color: button_two ? Colors.lightBlueAccent : Colors.grey,
-                        onPressed: () {
-                          setState(() {
-                            button_two = !button_two;
-                          });
-                        },
-                        child: Text("Festival"),
-                      )),
-                ],
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Icon(
+                              Icons.lock,
+                              size: 15.0,
+                              color: Colors.blue,
+                            ),
+                            Text(
+                              "Quiz",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-            Container(
-                alignment: Alignment.centerLeft,
-                margin: EdgeInsets.only(top: 10.0,left: 20.0),
-                child: Text("Module Available ",style: TextStyle(fontWeight: FontWeight.bold),)),
+              Container(
+                  margin: EdgeInsets.all(12.0),
+                  child: ExpandableText(
+                    longText,
+                    expandText: 'show more',
+                    collapseText: 'show less',
+                    maxLines: 9,
+                    linkColor: Colors.blue,
+                    style: TextStyle(fontSize: 18.0),
+                    textAlign: TextAlign.justify,
+                  )),
+              Divider(
+                color: Colors.black,
+                endIndent: 10.0,
+                indent: 12.0,
+              ),
+              // Video Library.
+              Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.only(top: 10.0,left: 20.0),
+                  child: Text("Videos on Brahmins ",style: TextStyle(fontWeight: FontWeight.bold),)),
+              // Gallery Container
+              Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.only(top: 10.0,left: 20.0),
+                  child: Text("Gallery",style: TextStyle(fontWeight: FontWeight.bold),)),
 
-          ],
+            ],
+          ),
         ),
       ),
     );
